b6a42d8af02a469c2ff55300b7f7db3ae63431a7
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-03 15:57:15 +1200

    Final Commit

--- src/undofunction/UndoFunction.java
+++ src/undofunction/UndoFunction.java
@@ -5,17 +5,70 @@
  */
 package undofunction;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  *
- * @author 92020474
+ * @title UndoFunction.java
+ * @author James Mitchell 92020474
+ * @date 3/09/19
+ * 
  */
-public class UndoFunction {
 
-    /**
-     * @param args the command line arguments
-     */
+public class UndoFunction {
+    
+    private static final Scanner in = new Scanner(System.in);
+    private static String input;
+    private static final Stack<String> directionsList = new Stack();
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //obtaining user input
+        while (true){
+            System.out.println("Press D to enter new direction, U to cancel last entry and E to quit the program");
+            input = in.nextLine();
+            switch (input) {
+                case "D":
+                    getDirection();
+                    break;
+                case "U":
+                    undoDirection();
+                    break;
+                case "E":
+                    System.out.println("Bye...");
+                    System.exit(0);
+                default:
+                    System.out.println("The command is not recognised");
+                    break;
+            }
+        }
     }
+    
+    //this method removes last element from the stack with list of directions 
+    //if the stack is not empty
+    public static void undoDirection(){
+        if (directionsList.isEmpty()){
+            System.out.println("The list of directions is empty.");
+        }
+        else{
+            System.out.println(directionsList.peek() + " has been removed from the directions list");
+            directionsList.pop();
+            displayDirections();            
+        }
+    }
+    //this method adds an element to the stack with list of directions 
+    public static void getDirection(){
+        System.out.println("Type in the direction\n");
+        input = in.nextLine();
+        directionsList.push(input);  
+        displayDirections();
+    }  
+    //displays all the elements in the direction list
+    public static void displayDirections(){
+        System.out.println("LIST OF DIRECTIONS: ");
+        directionsList.forEach((s) -> {
+            System.out.println(s);
+        });
+    }   
     
 }
